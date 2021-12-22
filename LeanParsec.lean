233cf2432f4536e5027b1b852cccc66e3948d396
James King <james@agentultra.com>
2021-12-22 12:39:39 -0500

    Add the Parser type
    A minimal, basic definition and a Functor instance.

--- LeanParsec.lean
+++ LeanParsec.lean
@@ -1,1 +1,14 @@
-def hello := "world"
+structure Parser (α : Type u) where
+  runParser : ByteArray → OptionM (ByteArray × α)
+
+namespace Parser
+
+def map {α β : Type u} (f : α → β) (p : Parser α) : Parser β :=
+  Parser.mk $ fun input => do
+    let (input', p') ← p.runParser input
+    return (input', f p')
+
+instance : Functor Parser where
+  map := Parser.map
+
+end Parser
